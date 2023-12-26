383be1b8d29b5fa0dbc85054038905e09fd247e1
s-sakshi9317 <141143973+s-sakshi9317@users.noreply.github.com>
2023-12-26 23:42:40 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,266 +1,172 @@
 import React from "react";
 
-import { useNavigate } from "react-router-dom";
+import { Img, List, Text } from "components";
 
-import { Button, Img, Line, Text } from "components";
-
-const DesktopTwoPage = () => {
-  const navigate = useNavigate();
-
+const FrameThreePage = () => {
   return (
     <>
-      <div className="bg-blue_gray-900 flex flex-col font-inter items-center justify-start mx-auto pb-8 w-full">
-        <header
-          className="bg-cover bg-no-repeat flex md:flex-col md:gap-5 h-[122px] items-center justify-center md:px-5 w-full"
-          style={{ backgroundImage: "url('images/img_group11.svg')" }}
-        >
-          <div className="flex md:flex-1 flex-col items-center justify-start mb-[11px] mr-10 md:mt-0 mt-[17px] w-[98%] md:w-full">
-            <div className="flex md:flex-col flex-row md:gap-5 items-end justify-start w-full">
-              <Img
-                className="md:flex-1 h-[87px] sm:h-auto mb-1.5 object-cover w-[12%] md:w-full"
-                src="images/img_sihlogoremovebgpreview.png"
-                alt="sihlogoremovebg"
-              />
-              <div
-                className="common-pointer flex md:flex-1 flex-col gap-2.5 items-center justify-start md:ml-[0] ml-[863px] md:mt-0 mt-[9px] w-[5%] md:w-full"
-                onClick={() => navigate("/")}
-              >
-                <Img
-                  className="h-[50px] md:h-auto rounded-[50%] w-[50px]"
-                  src="images/img_home1.png"
-                  alt="homeOne"
-                />
-                <Text
-                  className="text-black-900 text-lg tracking-[1.80px]"
-                  size="txtInterExtraBold18"
-                >
-                  Home
-                </Text>
-              </div>
-              <div className="flex md:flex-1 flex-col gap-3 items-center justify-start ml-5 md:ml-[0] md:mt-0 mt-3 w-[9%] md:w-full">
-                <Img
-                  className="h-[47px] md:h-auto object-cover w-[46px] sm:w-full"
-                  src="images/img_language1.png"
-                  alt="languageOne"
-                />
-                <Text
-                  className="text-black-900 text-lg tracking-[1.80px]"
-                  size="txtInterExtraBold18"
-                >
-                  Languages
-                </Text>
-              </div>
-              <div className="flex md:flex-1 flex-col gap-2.5 items-center justify-start ml-5 md:ml-[0] md:mt-0 mt-[9px] w-[7%] md:w-full">
-                <Img
-                  className="h-[50px] md:h-auto object-cover w-[42%] sm:w-full"
-                  src="images/img_telephone1.png"
-                  alt="telephoneOne"
-                />
-                <Text
-                  className="text-black-900 text-lg tracking-[1.80px]"
-                  size="txtInterExtraBold18"
-                >
-                  Contact
-                </Text>
-              </div>
-              <div
-                className="common-pointer flex md:flex-1 flex-col gap-[11px] items-center justify-start ml-5 md:ml-[0] md:mt-0 mt-[17px] w-[5%] md:w-full"
-                onClick={() => navigate("/login")}
-              >
-                <Img
-                  className="h-[42px] md:h-auto object-cover w-[42px]"
-                  src="images/img_userinterface1.png"
-                  alt="userinterfaceOne"
-                />
-                <a
-                  href="javascript:"
-                  className="text-black-900 text-lg tracking-[1.80px]"
-                >
-                  <Text size="txtInterExtraBold18">Log in</Text>
-                </a>
-              </div>
-            </div>
-          </div>
+      <div className="bg-white-A700 flex flex-col font-imfellfrenchcanonsc items-center justify-start mx-auto p-[38px] sm:px-5 w-full">
+        <div className="flex flex-col gap-[47px] justify-start max-w-[1363px] mb-[89px] mt-[33px] mx-auto md:px-5 w-full">
           <Text
-            className="mb-[39px] md:ml-[0] ml-[127px] mr-[1112px] md:mt-0 mt-[43px] md:text-3xl sm:text-[28px] text-[32px] text-cyan-700 tracking-[3.20px]"
-            size="txtInterExtraBold32"
+            className="md:ml-[0] ml-[157px] md:text-5xl text-[86.55px] text-black-900"
+            size="txtIMFELLFrenchCanonSC8655"
           >
-            RescueNet
+            SAFETY INFORMATION
           </Text>
-        </header>
-        <div className="bg-blue_gray-900 flex md:flex-col flex-row md:gap-5 items-center justify-evenly overflow-auto w-full">
-          <div className="h-[107px] md:px-5 relative w-1/5 md:w-full">
-            <div className="absolute bg-blue_gray-900 h-[107px] inset-y-[0] my-auto right-[0] w-1/2"></div>
-            <Text
-              className="common-pointer absolute h-max inset-y-[0] my-auto right-[0] text-center text-lg text-white-A700 tracking-[1.80px]"
-              size="txtInterMedium18"
-              onClick={() => navigate("/weather")}
+          <div className="flex flex-col font-inconsolata gap-[46px] items-center justify-start w-full">
+            <List
+              className="sm:flex-col flex-row md:gap-10 gap-[60px] grid sm:grid-cols-1 md:grid-cols-2 grid-cols-3 justify-center w-full"
+              orientation="horizontal"
             >
-              <>
-                Weather
-                <br /> Alert
-              </>
-            </Text>
-            <Text
-              className="absolute h-max inset-y-[0] left-[0] my-auto text-center text-lg text-white-A700 tracking-[1.80px]"
-              size="txtInterMedium18"
-            >
-              <>
-                Emergency
-                <br />
-                Alert
-              </>
-            </Text>
-          </div>
-          <div className="h-[107px] md:px-5 relative w-[10%] md:w-full">
-            <div className="absolute bg-blue_gray-900 h-[107px] inset-[0] justify-center m-auto w-full"></div>
-            <Text
-              className="common-pointer absolute h-max inset-[0] justify-center m-auto text-center text-lg text-white-A700 tracking-[1.80px]"
-              size="txtInterMedium18"
-              onClick={() => navigate("/info")}
-            >
-              <>
-                Safety
-                <br />
-                Information
-              </>
-            </Text>
-          </div>
-          <div className="bg-blue_gray-900 flex flex-col items-center justify-start p-[9px] md:px-5">
-            <Text
-              className="my-3.5 text-center text-lg text-white-A700 tracking-[1.80px]"
-              size="txtInterMedium18"
-            >
-              <>
-                Volunteer
-                <br />
-                Registration
-              </>
-            </Text>
-          </div>
-          <Button
-            className="cursor-pointer font-medium leading-[normal] min-w-[143px] text-center text-lg tracking-[1.80px]"
-            shape="square"
-          >
-            Response
-          </Button>
-          <Button
-            className="cursor-pointer font-medium leading-[normal] min-w-[143px] text-center text-lg tracking-[1.80px]"
-            shape="square"
-          >
-            Resources
-          </Button>
-          <div className="h-[107px] md:px-5 relative w-[10%] md:w-full">
-            <div className="absolute bg-blue_gray-900 h-[107px] inset-[0] justify-center m-auto w-full"></div>
-            <Text
-              className="absolute h-max inset-[0] justify-center m-auto text-center text-lg text-white-A700 tracking-[1.80px]"
-              size="txtInterMedium18"
-            >
-              <>
-                Donate/
-                <br />
-                contribute
-              </>
-            </Text>
-          </div>
-          <div className="h-[107px] sm:h-[234px] md:px-5 relative w-[30%] md:w-full">
-            <div className="absolute flex flex-col h-full inset-[0] items-center justify-center m-auto w-full">
-              <div className="flex sm:flex-col flex-row sm:gap-5 items-center justify-evenly w-full">
-                <div className="bg-blue_gray-900 h-[107px] w-[34%]"></div>
-                <div className="flex sm:flex-1 flex-row items-center justify-evenly w-[67%] sm:w-full">
-                  <div className="h-[107px] relative w-1/2">
-                    <div className="absolute bg-blue_gray-900 h-[107px] inset-[0] justify-center m-auto w-full"></div>
+              <div className="bg-blue_gray-900 flex flex-1 flex-col gap-[5px] justify-end sm:ml-[0] p-[13px] w-full">
+                <Img
+                  className="h-24 md:h-auto md:ml-[0] ml-[122px] mr-[159px] mt-1 object-cover w-[28%] sm:w-full"
+                  src="images/img_earthquake1.png"
+                  alt="earthquakeOne"
+                />
+                <Text
+                  className="md:ml-[0] ml-[27px] mr-[41px] sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700"
+                  size="txtInconsolataRegular577"
+                >
+                  EARTHQUAKES
+                </Text>
+              </div>
+              <div className="bg-blue_gray-900 flex flex-1 flex-col items-center justify-end sm:ml-[0] p-[17px] w-full">
+                <Img
+                  className="h-24 md:h-auto object-cover w-[28%] sm:w-full"
+                  src="images/img_landslide2.png"
+                  alt="landslideOne"
+                />
+                <Text
+                  className="sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700"
+                  size="txtInconsolataRegular577"
+                >
+                  LANDSLIDES
+                </Text>
+              </div>
+              <div className="bg-blue_gray-900 flex flex-1 flex-col items-center justify-end sm:ml-[0] p-[17px] w-full">
+                <Img
+                  className="h-24 md:h-auto object-cover w-[28%] sm:w-full"
+                  src="images/img_floodedhouse2.png"
+                  alt="floodedhouseOne"
+                />
+                <Text
+                  className="sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700"
+                  size="txtInconsolataRegular577"
+                >
+                  FLOODS
+                </Text>
+              </div>
+            </List>
+            <div className="flex md:flex-col flex-row md:gap-10 items-center justify-between w-full">
+              <div className="bg-blue_gray-900 flex md:flex-1 flex-col gap-[9px] items-center justify-start p-2 w-[31%] md:w-full">
+                <Img
+                  className="h-24 md:h-auto object-cover w-[27%] sm:w-full"
+                  src="images/img_image7.png"
+                  alt="imageOne"
+                />
+                <Text
+                  className="mb-[9px] sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700"
+                  size="txtInconsolataRegular577"
+                >
+                  TSUNAMIS
+                </Text>
+              </div>
+              <div className="bg-blue_gray-900 h-[194px] p-[18px] relative w-[31%] md:w-full">
+                <Img
+                  className="h-24 mb-[-8.31px] ml-auto mr-[121px] mt-2 object-cover w-[26%] z-[1]"
+                  src="images/img_image8.png"
+                  alt="imageTwo"
+                />
+                <Text
+                  className="mt-auto mx-auto sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700"
+                  size="txtInconsolataRegular577"
+                >
+                  LIGHTNING
+                </Text>
+              </div>
+              <div className="bg-blue_gray-900 flex md:flex-1 flex-col items-center justify-start p-4 w-[31%] md:w-full">
+                <Img
+                  className="h-24 md:h-auto object-cover w-[28%]"
+                  src="images/img_image9.png"
+                  alt="imageThree"
+                />
+                <div className="h-[61px] relative w-[69%]">
+                  <Text
+                    className="m-auto sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700"
+                    size="txtInconsolataRegular577"
+                  >
+                    TORNADOES
+                  </Text>
+                  <div className="absolute h-[61px] inset-[0] justify-center m-auto w-full">
                     <Text
-                      className="absolute h-max inset-[0] justify-center m-auto text-center text-lg text-white-A700 tracking-[1.80px]"
-                      size="txtInterMedium18"
+                      className="m-auto sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700"
+                      size="txtInconsolataRegular577"
                     >
-                      <>
-                        Emergency
-                        <br />
-                        Contacts
-                      </>
+                      TORNADOES
                     </Text>
+                    <div className="absolute h-[61px] inset-[0] justify-center m-auto w-full">
+                      <Text
+                        className="m-auto sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700"
+                        size="txtInconsolataRegular577"
+                      >
+                        TORNADOES
+                      </Text>
+                      <Text
+                        className="absolute h-full inset-[0] justify-center m-auto sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700 w-max"
+                        size="txtInconsolataRegular577"
+                      >
+                        TORNADOES
+                      </Text>
+                    </div>
                   </div>
-                  <div className="bg-blue_gray-900 h-[107px] w-1/2"></div>
                 </div>
               </div>
             </div>
-            <Text
-              className="absolute h-max inset-y-[0] left-[0] my-auto text-center text-lg text-white-A700 tracking-[1.80px]"
-              size="txtInterMedium18"
+            <List
+              className="sm:flex-col flex-row md:gap-10 gap-[60px] grid sm:grid-cols-1 md:grid-cols-2 grid-cols-3 justify-center w-full"
+              orientation="horizontal"
             >
-              <>
-                Report
-                <br />
-                Incident
-              </>
-            </Text>
-            <Text
-              className="absolute h-max inset-y-[0] my-auto right-[0] text-center text-lg text-white-A700 tracking-[1.80px]"
-              size="txtInterMedium18"
-            >
-              <>
-                Find nearby
-                <br />
-                Agencies
-              </>
-            </Text>
-          </div>
-        </div>
-        <Line className="bg-white-A700 h-px max-w-[1418px] mx-auto w-full" />
-        <div className="backdrop-opacity-[0.5] bg-cyan-50_01 blur-[4.00px] flex flex-col font-inder gap-11 items-center justify-start mt-[35px] p-[13px] md:px-5 rounded-[30px] w-[24%] md:w-full">
-          <Text
-            className="text-black-900 text-xl tracking-[2.00px]"
-            size="txtInderRegular20"
-          >
-            Select any one{" "}
-          </Text>
-          <div className="flex flex-col items-center justify-start mb-[67px] w-[85%] md:w-full">
-            <div className="h-[74px] relative w-full">
-              <div className="bg-blue_gray-900 h-[74px] m-auto rounded-[133px] shadow-bs w-full"></div>
-              <Text
-                className="absolute bottom-[16%] left-1/4 text-3xl sm:text-[26px] md:text-[28px] text-gray-50_01 tracking-[3.00px]"
-                size="txtInderRegular30"
-              >
-                1.Hindi
-              </Text>
-            </div>
-            <div className="md:h-[125px] h-[74px] mt-[51px] relative w-full">
-              <div className="bg-blue_gray-900 h-[74px] m-auto rounded-[133px] shadow-bs w-full"></div>
-              <Text
-                className="absolute bottom-[20%] left-[17%] text-3xl sm:text-[26px] md:text-[28px] text-gray-50_02 tracking-[3.00px]"
-                size="txtInderRegular30Gray5002"
-              >
-                2.English
-              </Text>
-            </div>
-            <div className="md:h-[120px] h-[74px] mt-[46px] relative w-full">
-              <div className="bg-blue_gray-900 h-[74px] m-auto rounded-[133px] shadow-bs w-full"></div>
-              <Text
-                className="absolute bottom-[20%] inset-x-[0] mx-auto text-3xl sm:text-[26px] md:text-[28px] text-white-A700 tracking-[3.00px] w-max"
-                size="txtInderRegular30WhiteA700"
-              >
-                3.Malayalam
-              </Text>
-            </div>
-            <div className="md:h-[123px] h-[74px] mt-[49px] relative w-full">
-              <div className="bg-blue_gray-900 h-[74px] m-auto rounded-[133px] shadow-bs w-full"></div>
-              <Text
-                className="absolute h-max inset-y-[0] left-[19%] my-auto text-3xl sm:text-[26px] md:text-[28px] text-gray-50_03 tracking-[3.00px]"
-                size="txtInderRegular30Gray5003"
-              >
-                4.Gujrati{" "}
-              </Text>
-            </div>
-            <div className="md:h-[120px] h-[74px] mt-[46px] relative w-full">
-              <div className="bg-blue_gray-900 h-[74px] m-auto rounded-[133px] shadow-bs w-full"></div>
-              <Text
-                className="absolute bottom-[19%] inset-x-[0] mx-auto text-3xl sm:text-[26px] md:text-[28px] text-white-A700 tracking-[3.00px] w-max"
-                size="txtInderRegular30WhiteA700"
-              >
-                5.Punjabi
-              </Text>
-            </div>
+              <div className="bg-blue_gray-900 flex flex-1 flex-col gap-[9px] justify-start p-2 w-full">
+                <Img
+                  className="h-24 md:h-auto md:ml-[0] ml-[127px] object-cover w-[27%] sm:w-full"
+                  src="images/img_image4.png"
+                  alt="imageFour"
+                />
+                <Text
+                  className="mb-[9px] md:ml-[0] ml-[59px] mr-[77px] sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700"
+                  size="txtInconsolataRegular577"
+                >
+                  WILDFIRES
+                </Text>
+              </div>
+              <div className="bg-blue_gray-900 flex flex-1 flex-col gap-[9px] items-center justify-start p-2 w-full">
+                <Img
+                  className="h-24 md:h-auto object-cover w-[27%] sm:w-full"
+                  src="images/img_image5.png"
+                  alt="imageFive"
+                />
+                <Text
+                  className="mb-[9px] sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700"
+                  size="txtInconsolataRegular577"
+                >
+                  HURRICANES
+                </Text>
+              </div>
+              <div className="bg-blue_gray-900 flex flex-1 flex-col gap-[9px] items-center justify-start p-[9px] w-full">
+                <Img
+                  className="h-24 md:h-auto object-cover w-[27%] sm:w-full"
+                  src="images/img_image6.png"
+                  alt="imageSix"
+                />
+                <Text
+                  className="mb-[9px] sm:text-[43.7px] md:text-[49.7px] text-[57.7px] text-white-A700"
+                  size="txtInconsolataRegular577"
+                >
+                  EXTREME HEAT
+                </Text>
+              </div>
+            </List>
           </div>
         </div>
       </div>
@@ -268,4 +174,4 @@ const DesktopTwoPage = () => {
   );
 };
 
-export default DesktopTwoPage;
+export default FrameThreePage;
